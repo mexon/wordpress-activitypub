e261f99560be9d013a5f754f218eae47a03f5104
Matthew Exon <git.mexon@spamgourmet.com>
2023-01-07 15:35:14 +0100

    configuration item for number of images to attach

--- includes/model/class-post.php
+++ includes/model/class-post.php
@@ -76,7 +76,7 @@ class Post {
 	}
 
 	public function generate_attachments() {
-		$max_images = \apply_filters( 'activitypub_max_images', 3 );
+		$max_images = \apply_filters( 'activitypub_max_images', \get_option( 'activitypub_allowed_html', ACTIVITYPUB_NUMBER_IMAGES ) );
 
 		$images = array();
 
