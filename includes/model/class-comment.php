bdb3343ac6a4dc37f7a1af0ee5679b6d5deebab4
Matthew Exon <git.mexon@spamgourmet.com>
2023-01-11 20:13:07 +0100

    support for attribution

--- includes/model/class-comment.php
+++ includes/model/class-comment.php
@@ -13,8 +13,12 @@ class Comment {
 	public function __construct( $comment = null ) {
 		$this->comment = \get_comment( $comment );
 
-		$this->comment_author = $this->comment->user_id;
+		$this->parent_post = \WP_Post::get_instance( $this->comment->comment_post_ID );
+		$this->parent_comment = \WP_Comment::get_instance( $this->comment->comment_parent );
+
+		$this->comment_author = $this->generate_comment_author();
 		$this->id             = $this->generate_id();
+		$this->inReplyTo      = $this->generate_in_reply_to();
 		$this->content        = $this->generate_the_content();
 		$this->object_type    = $this->generate_object_type();
 	}
@@ -39,6 +43,8 @@ class Comment {
 			'type' => $this->object_type,
 			'published' => \gmdate( 'Y-m-d\TH:i:s\Z', \strtotime( $comment->comment_date_gmt ) ),
 			'content' => $this->content,
+			'inReplyTo' => $this->inReplyTo,
+			'attributedTo' => $this->comment_author,
 		);
 
 		return \apply_filters( 'activitypub_comment', $array );
@@ -48,12 +54,37 @@ class Comment {
 		return \wp_json_encode( $this->to_array(), \JSON_HEX_TAG | \JSON_HEX_AMP | \JSON_HEX_QUOT );
 	}
 
-	public function generate_id() {
-		$comment      = $this->comment;
-		$permalink = \get_comment_link( $comment );
-
+	public function generate_id_for_permalink( $permalink ) {
 		// replace 'trashed' for delete activity
 		return \str_replace( '__trashed', '', $permalink );
+	}
+
+	public function generate_id_for_comment( $comment ) {
+		return $this->generate_id_for_permalink( \get_comment_link( $comment ) );
+	}
+
+	public function generate_id_for_post( $post ) {
+		return $this->generate_id_for_permalink( \get_permalink( $post ) );
+	}
+
+	public function generate_id() {
+		return $this->generate_id_for_comment( $this->comment );
+	}
+
+	public function generate_comment_author() {
+		if ( $this->comment->user_id != 0 ) {
+			\get_author_posts_url( $this->comment->user_id );
+		}
+		return $this->comment->author_url;
+	}
+
+	public function generate_in_reply_to() {
+		if ( $this->parent_comment ) {
+			return $this->generate_id_for_comment( $this->parent_comment );
+		}
+		if ( $this->parent_post ) {
+			return $this->generate_id_for_post( $this->parent_post );
+		}
 	}
 
 	/**
@@ -66,7 +97,7 @@ class Comment {
 	}
 
 	public function generate_the_content() {
-                $content = $this->comment->comment_content;
+		$content = $this->comment->comment_content;
 
 		$content = \trim( \preg_replace( '/[\r\n]{2,}/', '', $content ) );
 
@@ -89,17 +120,16 @@ class Comment {
 	 */
 	private function get_thread_author_ids_from_comment( $comment ) {
 		\error_log( "@@@ get_thread_author_ids, user " . $comment->user_id . ", parent " . $this->comment->comment_parent );
-		$parent_comment = \WP_Comment::get_instance( $this->comment->comment_parent );
-		if ( $parent_comment ) {
-			$author_ids = $this->get_thread_author_ids_from_comment( $parent_comment );
+		if ( $this->parent_comment ) {
+			$author_ids = $this->get_thread_author_ids_from_comment( $this->parent_comment );
 		}
 		else {
 			$author_ids = [];
 		}
-                if ( $comment->user_id ) {
-                	array_push( $author_ids, $comment->user_id );
-                }
-                return $author_ids;
+		if ( $comment->user_id ) {
+			array_push( $author_ids, $comment->user_id );
+		}
+		return $author_ids;
 	}
 
 	/**
@@ -109,12 +139,11 @@ class Comment {
 	 */
 	public function get_thread_author_ids() {
 		\error_log( "@@@ get_thread_author_ids post id " . $this->comment->comment_post_ID );
-                $author_ids = $this->get_thread_author_ids_from_comment( $this->comment );
-		$parent_post = \WP_Post::get_instance( $this->comment->comment_post_ID );
-		if ( $parent_post ) {
-			array_push( $author_ids, $parent_post->post_author );
+		$author_ids = $this->get_thread_author_ids_from_comment( $this->comment );
+		if ( $this->parent_post ) {
+			array_push( $author_ids, $this->parent_post->post_author );
 		}
 		\error_log( "@@@ get_thread_author_ids " . print_r( $author_ids, true ) );
-                return $author_ids;
+		return $author_ids;
 	}
 }
